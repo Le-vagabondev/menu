8b9db8f210627bb6ee538b170d8dc78f95be8cf2
Le-vagabondev <75924500+Le-vagabondev@users.noreply.github.com>
2021-02-26 16:43:52 -0500

    eventos iniciados

--- dist/scripts.dev.js
+++ dist/scripts.dev.js
@@ -1,6 +1,7 @@
-'use strict';
+'use strict'; //gsap.to("", {duration: 1, x: 500});
 
-gsap.to("", {
-  duration: 1,
-  x: 500
-});
+var menuNav = document.getElementById('nav');
+
+var list = function list() {
+  menuNav.addEventListener('click', function () {});
+};
--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
 'use strict'
-
-gsap.to("", {duration: 1, x: 500});
+const menubotn = document.getElementById('nav')
+console.log('prueba')
+   menubotn.addEventListener('click', e => {
+       if(!menubotn) return
+       const t = e.target,
+             d = t.dataset
+             let caja = document.getElementById('menu')
+             let caja2 = document.getElementById('men2')
+             if(d.uno) {console.log('btn1')
+            caja2.style.display= 'none';
+            caja.style.display= 'block';
+            }
+             if(d.dos) {console.log('btn2')
+            caja.style.display= 'none';
+            caja2.style.display= 'block';
+            }
+             
+})
