ea5d67d6155c0ff60253fa7b25bc142874a1f409
Le-vagabondev <75924500+Le-vagabondev@users.noreply.github.com>
2021-03-22 01:10:33 -0500

    definir_carrousel

--- dist/scripts.dev.js
+++ dist/scripts.dev.js
@@ -1,7 +1,33 @@
-'use strict'; //gsap.to("", {duration: 1, x: 500});
+'use strict';
 
-var menuNav = document.getElementById('nav');
+var menubotn = document.getElementById('nav');
+console.log('prueba');
+menubotn.addEventListener('click', function (e) {
+  if (!menubotn) return;
+  var t = e.target,
+      d = t.dataset;
+  var caja = document.getElementById('menu');
+  var caja2 = document.getElementById('men2');
+  var caja3 = document.getElementById('men3');
 
-var list = function list() {
-  menuNav.addEventListener('click', function () {});
-};
+  if (d.uno) {
+    console.log('btn1');
+    caja2.style.display = 'none';
+    caja3.style.display = 'none';
+    caja.style.display = 'block';
+  }
+
+  if (d.dos) {
+    console.log('btn2');
+    caja.style.display = 'none';
+    caja2.style.display = 'block';
+    caja3.style.display = 'none';
+  }
+
+  if (d.tres) {
+    console.log('btn3');
+    caja.style.display = 'none';
+    caja3.style.display = 'block';
+    caja2.style.display = 'none';
+  }
+});
--- scripts.js
+++ scripts.js
@@ -7,13 +7,21 @@ console.log('prueba')
              d = t.dataset
              let caja = document.getElementById('menu')
              let caja2 = document.getElementById('men2')
+             let caja3 = document.getElementById('men3')
              if(d.uno) {console.log('btn1')
             caja2.style.display= 'none';
+            caja3.style.display= 'none';
             caja.style.display= 'block';
             }
              if(d.dos) {console.log('btn2')
             caja.style.display= 'none';
             caja2.style.display= 'block';
+            caja3.style.display= 'none';
+            }
+            if(d.tres) {console.log('btn3')
+            caja.style.display= 'none';
+            caja3.style.display= 'block';
+            caja2.style.display= 'none';
             }
              
 })
